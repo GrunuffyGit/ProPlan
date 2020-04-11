bd889ed2d434bbc78723e4fb21b2f75d4a79e347
GrunuffyGit <Gloria.wm.tam@gmail.com>
2020-04-10 21:20:27 -0700

    try to add db

---
+++ dbConfig.js
@@ -0,0 +1,20 @@
+const Pool = require('pg').Pool;
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgres://localhost:5432/booktonica`
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: true
+})
+class dbFunctions{
+    testApi = function(req, res){
+        pool.query("SELECT * FROM users;", (error,result)=>{
+            if(error){
+                console.log(error);
+            }else{
+                res.status(200).json(result.rows);
+            }
+        })
+    }
+}
+
+module.exports = {dbFunctions};
--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+const db = require("./dbConfig.js");
+const dbFunc = new db.dbFunctions();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'react-client/build')));
 
-app.get('*', (_, res) => {
+app.get('/', (_, res) => {
     res.sendFile(path.join(__dirname+'/react-client/build/index.html'));
 });
+
+app.get("/test", dbFunc.testApi)
 
 var server = app.listen(process.env.PORT || 3001, function(){
     var host = server.address().address;
