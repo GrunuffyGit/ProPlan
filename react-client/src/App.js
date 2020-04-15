91d2af76b14a8593e496dec630a08a17320f5ae6
GrunuffyGit <Gloria.wm.tam@gmail.com>
2020-04-14 18:46:42 -0700

    testing grid

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -4,25 +4,72 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      columnDefs: [{
-        headerName: "Make", field: "make"
-      }, {
-        headerName: "Model", field: "model"
-      }, {
-        headerName: "Price", field: "price"
-      }],
-      rowData: [{
-        make: "Toyota", model: "Celica", price: 35000
-      }, {
-        make: "Ford", model: "Mondeo", price: 32000
-      }, {
-        make: "Porsche", model: "Boxter", price: 72000
-      }]
+      columnDefs: [
+      {
+        headerName: "Time", 
+        field: "time"
+      }, 
+      {
+        headerName: "Monday 2/12", 
+        field: "monday"
+      }, 
+      {
+        headerName: "Tuesday 2/13", 
+        field: "tuesday"
+      },
+      {
+        headerName: "Wednesday 2/14", 
+        field: "wednesday"
+      },
+    ],
+      rowData: [
+        {
+        time: "12:00 AM", 
+        monday: "Celica", 
+        tuesday: 35000,
+        wednesday: "poop"
+      }, 
+      {
+        time: "1:00 AM", 
+        monday: "Mondeo", 
+        tuesday: 32000,
+        wednesday: "poop"
+      }, 
+      {
+        time: "2:00 AM", 
+        monday: "Boxter", 
+        tuesday: 72000,
+        wednesday: "poop"
+      },
+      {
+        time: "3:00 AM", 
+        monday: "Boxter", 
+        tuesday: 72000,
+        wednesday: "poop"
+      },
+      {
+        time: "4:00 AM", 
+        monday: "Boxter", 
+        tuesday: 72000,
+        wednesday: "poop"
+      },
+      {
+        time: "5:00 AM", 
+        monday: "Boxter", 
+        tuesday: 72000,
+        wednesday: "poop"
+      },
+    ]
     }
+  }
+
+  onGridReady = params => {
+    this.gridColumnApi = params.columnApi;
+    this.gridColumnApi.setColumnPinned("time", "left");
   }
 
   render() {
@@ -30,12 +77,14 @@ class App extends Component {
       <div
         className="ag-theme-balham"
         style={{
-        height: '500px',
-        width: '600px' }}
+        height: '20vh',
+        width: '50vw' }}
       >
         <AgGridReact
           columnDefs={this.state.columnDefs}
-          rowData={this.state.rowData}>
+          rowData={this.state.rowData}
+          onGridReady={this.onGridReady}
+          >
         </AgGridReact>
       </div>
     );
